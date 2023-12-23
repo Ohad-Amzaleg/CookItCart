78bed0ea24c052d6a8e51cf1013af4b5022a5c54
ohadAM12 <ohadamz@post.bgu.ac.il>
2023-12-23 16:03:19 +0200

    ver

--- FrontEnd/my-react-app/src/Classes/Schedule.ts
+++ FrontEnd/my-react-app/src/Classes/Schedule.ts
@@ -1,24 +1,24 @@
-import { BASE_URL } from "../constants";
-import axios, { AxiosResponse } from "axios";
+import { BASE_URL } from '../constants'
+import axios, { AxiosResponse } from 'axios'
 
 export default class Schedule {
-  email: string = "";
-  events: any[] = [];
-  weeklyNutrition: any = {};
+  email: string = ''
+  events: any[] = []
+  weeklyNutrition: any = {}
 
   constructor(email: string) {
-    this.email = email;
+    this.email = email
   }
 
   private async fetchData(endpoint: string): Promise<any> {
     try {
       const res = await axios.get(`${BASE_URL}/api/schedule/${endpoint}`, {
         withCredentials: true,
-      });
-      return res.data;
+      })
+      return res.data
     } catch (err) {
       // Handle error based on application requirements
-      throw new Error(`Error fetching ${endpoint}: ${err}`);
+      throw new Error(`Error fetching ${endpoint}: ${err}`)
     }
   }
 
@@ -28,40 +28,40 @@ export default class Schedule {
         `${BASE_URL}/api/schedule/${endpoint}`,
         { data },
         { withCredentials: true }
-      );
-      return res.data;
+      )
+      return res.data
     } catch (err) {
-      throw new Error(`Error posting ${endpoint}: ${err}`);
+      throw new Error(`Error posting ${endpoint}: ${err}`)
     }
   }
 
   async fetchEvents(): Promise<any[]> {
-    const data = await this.fetchData("events");
-    this.events = data.events ? data.events : [];
-    return this.events;
+    const data = await this.fetchData('events')
+    this.events = data.events ? data.events : []
+    return this.events
   }
 
   async fetchNutrition(): Promise<any[]> {
-    const data = await this.fetchData("nutrition");
-    this.weeklyNutrition = data.nutrition ? data.nutrition : {};
-    return this.weeklyNutrition;
+    const data = await this.fetchData('nutrition')
+    this.weeklyNutrition = data.nutrition ? data.nutrition : {}
+    return this.weeklyNutrition
   }
 
   async updateEvents(events: any[]): Promise<any> {
     try {
-      this.events = await this.postData("update/events", events);
-      return this.events;
+      this.events = await this.postData('update/events', events)
+      return this.events
     } catch (err) {
-      throw new Error(`Error updating schedule: ${err}`);
+      throw new Error(`Error updating schedule: ${err}`)
     }
   }
 
   async updateNutrition(nutrition: any[]): Promise<any> {
     try {
-      this.weeklyNutrition = await this.postData("update/nutrition", nutrition);
-      return this.weeklyNutrition;
+      this.weeklyNutrition = await this.postData('update/nutrition', nutrition)
+      return this.weeklyNutrition
     } catch (err) {
-      throw new Error(`Error updating nutrition: ${err}`);
+      throw new Error(`Error updating nutrition: ${err}`)
     }
   }
 }
