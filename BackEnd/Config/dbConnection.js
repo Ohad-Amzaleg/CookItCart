1cc0faa9976f1e20874cc037c5f982d19a0825aa
ohadAM12 <ohadamz@post.bgu.ac.il>
2023-12-23 17:40:42 +0200

    latest-ver

--- BackEnd/Config/dbConnection.js
+++ BackEnd/Config/dbConnection.js
@@ -1,19 +1,19 @@
-const mongoose = require('mongoose');
-require('dotenv').config();
+const mongoose = require('mongoose')
+require('dotenv').config()
 
-const connectionString = process.env.MONGODB_URI;
+const connectionString = process.env.MONGODB_URI
 const connectionParams = {
-	useUnifiedTopology: true,
-};
+  useUnifiedTopology: true,
+}
 
 const dbConnect = () => {
-	console.log(connectionString);
-	mongoose
-		.connect(connectionString, connectionParams)
-		.then(() => console.log('Connected to MongoDB...'))
-		.catch(err => {
-			console.log(err);
-			console.error('Could not connect to MongoDB...');
-		});
-};
-module.exports = { dbConnect };
+  console.log(connectionString)
+  mongoose
+    .connect(connectionString, connectionParams)
+    .then(() => console.log('Connected to MongoDB...'))
+    .catch((err) => {
+      console.log(err)
+      console.error('Could not connect to MongoDB...')
+    })
+}
+module.exports = { dbConnect }
